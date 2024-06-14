5d70810315fdbde5ef62b2bee07a4da02a4e02cd
Lautaro Colella <lautarocolella1@gmail.com>
2024-06-13 21:33:31 -0300

    project creation v0.5

---
+++ morse_practice.py
@@ -0,0 +1,110 @@
+from pydub import AudioSegment
+import configparser
+import os
+
+
+def read_config(file_path):
+    config = configparser.ConfigParser()
+    config.read(file_path)
+
+    level = config.get("settings", "level")
+
+    level_config_map = {
+        "beginner": {
+            "delay_between_sounds": 3,
+            "delay_between_characters": 5,
+            "delay_between_words": 9,
+        },
+        "intermediate": {
+            "delay_between_sounds": 1,
+            "delay_between_characters": 5,
+            "delay_between_words": 9,
+        },
+        "advanced": {
+            "delay_between_sounds": 1,
+            "delay_between_characters": 3,
+            "delay_between_words": 7,
+        },
+    }
+
+    if level == "arbitrary":
+        delay_between_sounds = config.getint(
+            "settings", "arbitrary_delay_between_sounds"
+        )
+        delay_between_characters = config.getint(
+            "settings", "arbitrary_delay_between_characters"
+        )
+        delay_between_words = config.getint("settings", "arbitrary_delay_between_words")
+
+        sound_delay_map = {
+            "delay_between_sounds": delay_between_sounds,
+            "delay_between_characters": delay_between_characters,
+            "delay_between_words": delay_between_words,
+        }
+    else:
+        if level not in level_config_map:
+            raise ValueError(
+                f"Invalid level '{level}' in config file. Choose from: arbitrary, {', '.join(level_config_map.keys())}"
+            )
+
+        sound_delay_map = level_config_map[level]
+
+    dot_sound_file = config.get("settings", "dot_sound_file")
+    dash_sound_file = config.get("settings", "dash_sound_file")
+    sound_map = {".": dot_sound_file, "-": dash_sound_file}
+
+    output_file = config.get("settings", "output_file")
+
+    word_separator = config.get("settings", "word_separator")
+
+    return sound_delay_map, sound_map, output_file, word_separator
+
+
+def generate_audio(
+    input_string, sound_delay_map, sound_map, output_file, word_separator
+):
+
+    silence_between_parts = AudioSegment.silent(
+        duration=len(sound_map["."]) * sound_delay_map["delay_between_sounds"] * 10
+    )
+    silence_between_letters = AudioSegment.silent(
+        duration=len(sound_map["."]) * sound_delay_map["delay_between_characters"] * 10
+    )
+    silence_between_words = AudioSegment.silent(
+        duration=len(sound_map["."]) * sound_delay_map["delay_between_words"] * 10
+    )
+
+    words = input_string.split(word_separator)
+    combined = AudioSegment.empty()
+
+    for word in words:
+        letters = word.strip().split()
+        for letter in letters:
+            for character in letter:
+                if character in sound_map:
+                    sound = AudioSegment.from_mp3(sound_map[character])
+                    combined += sound + silence_between_parts
+                else:
+                    print(f"Error: Invalid character '{character}'")
+                    return
+
+            combined += silence_between_letters
+        if word != words[len(words) - 1]:
+            combined += silence_between_words
+
+    combined.export(output_file, format="mp3")
+
+    print(f"Generated audio file: {output_file}")
+
+
+if __name__ == "__main__":
+
+    config_file = "config.cfg"
+
+    sound_delay_map, sound_map, output_file, word_separator = read_config(config_file)
+
+    input_string = input(f"Enter text in morse code, separated by '{word_separator}': ")
+
+    generate_audio(
+        input_string, sound_delay_map, sound_map, output_file, word_separator
+    )
---
+++ sounds/README.md
@@ -0,0 +1,90 @@
+# Morse Code Audio Generator
+
+This Python program generates an audio file (.mp3) from morse code text input based on specified configurations.
+
+## Installation
+
+1. **Clone the repository:**
+
+    ```sh
+    git clone <repository_url>
+    cd <repository_name>
+    ```
+
+2. **Install dependencies:**
+
+    ```sh
+    pip install pydub
+    ```
+
+    **IMPORTANT**: `pydub` requires ffmpeg to work, you have to [install it from the oficial website](https://ffmpeg.org/) and add it to your `PATH`.
+
+3. **Prepare configuration:**
+
+    Create or modify `config.cfg` file according to your preferences. See [Configuration Details](#configuration-details) below for more information.
+
+## Usage
+
+1. **Run the program:**
+
+    ```sh
+    python morse_code_generator.py
+    ```
+
+2. **Enter text input:**
+
+    Enter the Morse code text you wish to convert to audio, separated by the specified word separator (default is "/").
+
+3. **Output:**
+
+    The program will generate an audio file (`morse_output.mp3`) in the same directory.
+
+## Configuration Details
+
+The `config.cfg` file allows customization for Morse code audio generation
+
+- `dot_sound_file`: Path to the sound file for a dot.
+- `dash_sound_file`: Path to the sound file for a dash.
+- `output_file`: Path where the generated audio file will be saved.
+- `word_separator`: Separator used to split words in the input.
+- `level`: Specifies the Morse code timing level.
+
+**Level Settings:**
+
+- **beginner:** Suitable for beginners, with longer pauses.
+- **intermediate:** Uses Farnsworth Timing.
+- **advanced:** Standard Morse code timing.
+- **arbitrary:** Define custom delays
+
+For "arbitrary" level:
+
+- `arbitrary_delay_between_sounds`: Custom delay between sounds (`--.` is `- %DELAY% -.`)
+- `arbitrary_delay_between_characters`: Custom delay between characters (`- -.` is `- %DELAY% -.`)
+- `arbitrary_delay_between_words`: Custom delay between words. (`--. - / -.` is `--. - %DELAY% -.`)
+
+## Example Configuration
+
+```ini
+[settings]
+dot_sound_file = sounds/dot.mp3
+dash_sound_file = sounds/dash.mp3
+output_file = morse_output.mp3
+word_separator = /
+level = beginner
+
+arbitrary_delay_between_sounds = 9
+arbitrary_delay_between_characters = 9
+arbitrary_delay_between_words = 9
+```
+
+## LICENSE
+
+This project is under MIT license.
+
+Copyright 2024 Lautaro Colella
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
